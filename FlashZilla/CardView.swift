f2364579a9252cca6f8be3c96289dd59b43e1eb7
Dante Cesa <dantec@mac.com>
2022-02-23 23:38:39 -0800

    Added Accessibility Support

--- FlashZilla/CardView.swift
+++ FlashZilla/CardView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     let card: Card
     @State private var showingAnswer: Bool = false
     @State private var offset: CGSize = .zero
@@ -17,7 +18,18 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width) / 50))
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
@@ -47,7 +59,9 @@ struct CardView: View {
                     if abs(offset.width) > 100 {
                         removal?()
                     } else {
-                        offset = .zero
+                        withAnimation {
+                            offset = .zero
+                        }
                     }
                 }
         )
--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 
 extension View {
     func stacked(at position: Int, in total: Int) -> some View {
-        let offset = Double(position - total)
+        let offset = Double(total - position)
         return self.offset(x: 0, y: offset * 10)
     }
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State var cards: [Card] = Array<Card>.init(repeating: Card.example, count: 10)
     
     var body: some View {
@@ -29,10 +30,33 @@ struct ContentView: View {
                 ZStack {
                     ForEach(0..<cards.count, id:\.self) { index in
                         CardView(card: cards[index]) {
-                            removeCard(at: index)
+                            withAnimation {
+                                removeCard(at: index)
+                            }
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                }
+            }
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.75))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.75))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding(.horizontal, 50)
+                    .padding()
                 }
             }
         }
