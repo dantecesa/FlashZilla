4d0af8d8d284da3bfb1506070fedcccde08f62bc
Dante Cesa <dantec@mac.com>
2022-02-24 23:58:20 -0800

    Added empty state if the user has no cards

--- FlashZilla/Views/ContentView.swift
+++ FlashZilla/Views/ContentView.swift
@@ -28,92 +28,102 @@ struct ContentView: View {
     @State private var showingEditScreen: Bool = false
     
     var body: some View {
+        let noCards: Bool = DataManager.loadCards().count == 0
+        
         ZStack {
             Image(decorative: "background")
                 .ignoresSafeArea()
             
-            VStack {
-                Text("Time remaining: \(timeRemaining)")
-                    .font(.title2)
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 5)
-                    .background(.black.opacity(0.75))
+            if noCards {
+                Button("Add Cards", action: { showingEditScreen = true })
+                    .padding()
+                    .foregroundColor(.black)
+                    .background(.white)
                     .clipShape(Capsule())
-                ZStack {
-                    ForEach(Array(cards.enumerated()), id:\.element) { item in
-                        CardView(card: item.element) { reinsert in
-                            removeCard(at: item.offset, reinsert: reinsert)
-                        }
-                        .stacked(at: item.offset, in: cards.count)
-                        .allowsHitTesting(item.offset == cards.count - 1)
-                        .accessibilityHidden(item.offset < cards.count - 1)
-                    }
-                }
-                .allowsHitTesting(timeRemaining > 0)
-                
-                if cards.isEmpty {
-                    Button("Start Again?", action: resetGame)
-                        .padding()
-                        .foregroundColor(.black)
-                        .background(.white)
+            } else {
+                VStack {
+                    Text("Time remaining: \(timeRemaining)")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .background(.black.opacity(0.75))
                         .clipShape(Capsule())
-                }
-            }
-            
-            VStack {
-                HStack {
-                    Spacer()
+                    ZStack {
+                        ForEach(Array(cards.enumerated()), id:\.element) { item in
+                            CardView(card: item.element) { reinsert in
+                                removeCard(at: item.offset, reinsert: reinsert)
+                            }
+                            .stacked(at: item.offset, in: cards.count)
+                            .allowsHitTesting(item.offset == cards.count - 1)
+                            .accessibilityHidden(item.offset < cards.count - 1)
+                        }
+                    }
+                    .allowsHitTesting(timeRemaining > 0)
                     
-                    Button {
-                        showingEditScreen = true
-                    } label: {
-                        Label("Add Cards", systemImage: "plus.circle")
+                    if cards.isEmpty {
+                        Button("Start Again?", action: resetGame)
                             .padding()
-                            .background(.black.opacity(0.75))
+                            .foregroundColor(.black)
+                            .background(.white)
                             .clipShape(Capsule())
                     }
                 }
-                Spacer()
-            }
-            .foregroundColor(.white)
-            .font(.body)
-            .padding(.horizontal, 60)
-            .padding(.vertical, 40)
-            
-            if differentiateWithoutColor || voiceOverEnabled {
+
                 VStack {
-                    Spacer()
-                    
                     HStack {
-                        Button {
-                            removeCard(at: cards.count - 1, reinsert: true)
-                        } label: {
-                            Image(systemName: "xmark.circle")
-                                .padding()
-                                .background(.black.opacity(0.75))
-                                .clipShape(Circle())
-                        }
-                        .accessibilityLabel("Wrong")
-                        .accessibilityHint("Mark your answer incorrect.")
-                        
                         Spacer()
                         
                         Button {
-                            removeCard(at: cards.count - 1, reinsert: false)
+                            showingEditScreen = true
                         } label: {
-                            Image(systemName: "checkmark.circle")
+                            Label("Add Cards", systemImage: "plus.circle")
                                 .padding()
                                 .background(.black.opacity(0.75))
-                                .clipShape(Circle())
+                                .clipShape(Capsule())
                         }
-                        .accessibilityLabel("Correct")
-                        .accessibilityHint("Mark your answer correct.")
                     }
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .padding(.horizontal, 50)
-                    .padding()
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .font(.body)
+                .padding(.horizontal, 60)
+                .padding(.vertical, 40)
+                
+                if differentiateWithoutColor || voiceOverEnabled {
+                    VStack {
+                        Spacer()
+                        
+                        HStack {
+                            Button {
+                                removeCard(at: cards.count - 1, reinsert: true)
+                            } label: {
+                                Image(systemName: "xmark.circle")
+                                    .padding()
+                                    .background(.black.opacity(0.75))
+                                    .clipShape(Circle())
+                            }
+                            .accessibilityLabel("Wrong")
+                            .accessibilityHint("Mark your answer incorrect.")
+                            
+                            Spacer()
+                            
+                            Button {
+                                removeCard(at: cards.count - 1, reinsert: false)
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .padding()
+                                    .background(.black.opacity(0.75))
+                                    .clipShape(Circle())
+                            }
+                            .accessibilityLabel("Correct")
+                            .accessibilityHint("Mark your answer correct.")
+                        }
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(.horizontal, 50)
+                        .padding()
+                    }
                 }
             }
         }
